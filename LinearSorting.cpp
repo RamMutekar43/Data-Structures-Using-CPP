56fafd2849de042868d0410591e2b3bb92cd37c5
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2024-03-30 13:20:48 +0530

    Update LinearSorting.cpp

--- LinearSorting.cpp
+++ LinearSorting.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 int main(){
     int n;
-    // Prompt user to enter the length of the array
-    cout<<"Enter length of Array: "<<endl;
+    // Prompt user to enter the size of the Vector
+    cout<<"Enter size of Vector: "<<endl;
     cin>>n;
     vector<int>elementList(n);
-    // Prompt user to enter the elements of the array
-    cout<<"Enter elements of Array"<<endl;
+    // Prompt user to enter the elements of the Vector
+    cout<<"Enter elements of Vector"<<endl;
     for(int i=0;i<n;i++){
         int element;
         cin>>elementList[i];
     }
-    // Using for loop to sort elements in Array
+    // Using for loop to sort elements in Vector
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-1;j++){
             int  temp;
@@ -24,8 +24,8 @@ int main(){
             }
         }
     }
-    // Print sorted Array
-    cout<<"Sorted element Array:"<<endl;
+    // Print sorted Vector
+    cout<<"Sorted element are:"<<endl;
     for(int i=0;i<elementList.size();i++){
         cout<<elementList[i]<<" ";
     }
