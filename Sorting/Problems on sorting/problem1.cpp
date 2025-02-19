aa2e60d883f5965789379cb7cf9c4a0016744b2f
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-02-19 12:00:00 +0530

    Problem1

--- Sorting/Problems on sorting/problem1.cpp
+++ Sorting/Problems on sorting/problem1.cpp
@@ -7,15 +7,33 @@
 using namespace std;
 
 //finction to sort the elements
+
+
+// void sortElems(vector<int> &v){
+//     int n=v.size();
+//     bool flag =  false;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-1;j++){
+//             if(v[j]==0 && v[j+1]!=0){
+//                 swap(v[j],v[j+1]);
+//                 flag = true;
+//             }
+//         }
+//         if(!flag) break;
+//     }
+// }
+
 void sortElems(vector<int> &v){
     int n=v.size();
-    bool flag =  false;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-1;j++){
+    for(int i=n-1;i>+0;i--){
+        int j=0;
+        bool flag =  false;
+        while(j!=i){
             if(v[j]==0 && v[j+1]!=0){
                 swap(v[j],v[j+1]);
                 flag = true;
             }
+            j++;
         }
         if(!flag) break;
     }
