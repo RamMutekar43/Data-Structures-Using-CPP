1c9edd8782efba62fcc4ca5fe25be9cc1573a1b8
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-02-28 20:54:08 +0530

    Function Overriding

--- OOP/Polymorphism/FunctionOverriding.cpp
+++ OOP/Polymorphism/FunctionOverriding.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 class Parent{
     public:
+    // Declare a virtual function to enable polymorphism
     virtual void print(){
         cout<<"Parent Class"<<"\n";
     }
-
+    // Declare a non-virtual function
     void note(){
         cout<<"Hello! This is Parent Class"<<"\n";
     }
@@ -13,6 +14,7 @@ class Parent{
 
 class Child : public Parent{
     public:
+    // Override the print function from the Parent class
     void print(){
         cout<<"Child Class"<<"\n";
     }
@@ -22,11 +24,15 @@ class Child : public Parent{
     }
 };
 int main(){
-
+    // Declare a pointer to the Parent class
     Parent *p;
+    // Create an instance of the Child class
     Child c;
+    // Assign the address of the Child instance to the Parent pointer
     p = &c;
+    // Call the print function through the Parent pointer (polymorphism)
     p->print();
+    // Call the note function through the Parent pointer (not polymorphism)
     p->note();
 
     return 0;
