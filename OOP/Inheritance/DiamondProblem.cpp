6d9350d105103bfb71cdb7b55277249839dbb16d
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-02-28 08:19:31 +0530

    commit

---
+++ OOP/Inheritance/DiamondProblem.cpp
@@ -0,0 +1,47 @@
+// #include<iostream>
+// using namespace std;
+
+// class Parent{
+//     public:
+//     Parent(){
+//         cout<<"Parent class."<<endl;
+//     }
+// };
+
+// class Child1 : public Parent{
+//     public:
+//     Child1(){
+//         cout<<"Child1 class."<<endl;
+//     }
+// };
+// class Child2 : public Parent{
+//     public:
+//     Child2(){
+//         cout<<"Child2 class."<<endl;
+//     }
+// };
+
+// class A : public Child1{
+//     public:
+//     A(){
+//         cout<<"A class."<<endl;
+//     }
+// };
+// class B : public Child2{
+//     public:
+//     B(){
+//         cout<<"B class."<<endl;
+//     }
+// };
+
+// class GrandChild : public A, public B{
+//     public:
+//     GrandChild(){
+//         cout<<"GrandChild class."<<endl;
+//     }
+// };
+
+// int main(){
+//     GrandChild gc;
+//     return 0;
+// }
