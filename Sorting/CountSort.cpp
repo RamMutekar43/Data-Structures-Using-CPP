d134c286b33b0ecf7485fc8d49ff3921e432e777
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-03-06 23:26:45 +0530

    Count Sort

--- Sorting/CountSort.cpp
+++ Sorting/CountSort.cpp
@@ -1,7 +1,38 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int main(){
+void countSort(vector<int> arr){
+    int n = arr.size();
 
+    // calculate max elem
+    int maxElem = INT8_MIN;
+    for(int i=0;i<n;i++){
+        maxElem = max(arr[i],maxElem);
+    }
+
+    // create frequency array
+    vector<int> freq(maxElem+1,0);
+    for(int i=0;i<n;i++){
+        freq[arr[i]]++;
+    }
+
+    // cal cumulative frequency arr
+    for(int i=1;i<=maxElem;i++){
+        freq[i]+=freq[i-1];
+    }
+
+
+}
+
+int main(){
+    int n;
+    cout<<"enter sie of array:"<<"\n";
+    cin>>n;
+    vector<int>v(n);
+    countSort(v);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
     return 0;
 }
