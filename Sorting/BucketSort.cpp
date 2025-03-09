d07263c548bf8df0b45dbf71b01182d6b1d46859
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-03-09 10:19:20 +0530

    commit

--- Sorting/BucketSort.cpp
+++ Sorting/BucketSort.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+void bucketSort(float arr[], int n){
+    
+}
+
 int main(){
+    float arr[] = { 0.13, 0.45, 0.12, 0.89, 0.75, 0.63, 0.85, 0.39};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
