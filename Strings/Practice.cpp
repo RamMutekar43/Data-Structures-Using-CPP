40cdced50fbd788d54524e50369e4e6d5538f8d3
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-04-13 10:07:16 +0530

    strings

---
+++ Strings/Practice.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+
+    // string str="RAM";
+    // string str1("Mutekar Ram");
+    // cin>>str;
+    // cout<<str<<" "<<str1<<"\n";
+
+    // getline(cin,str);  // used to take sentence as a string input
+    // char ch = 'A' ;
+    // cout<<int(ch);
+
+    // string name = "RAM MUTEKAR";
+    // reverse(name.begin(),name.end());  // reverse function
+    // cout<<name
+    
+    // cout<<name.substr(0,3);   // substr(starting point , length of substring)  function to get substring
+
+    return 0;
+}
